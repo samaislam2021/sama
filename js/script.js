153dc95b1300058a10187d4016f40dceff0c1289
samaislam2021 <samaislam84@gmail.com>
2026-05-14 12:16:01 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Typing Script JS
 var typed = new Typed(".typing",{
-    strings: ["Web Designer", "Web Developer"],
+    strings: ["Web Designer"],
     typeSpeed: 80,
     backSpeed: 80,
 });
 
 var typed = new Typed(".typing-2",{
-    strings: ["Web Developer", "Web Designer"],
+    strings: ["Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
 });
